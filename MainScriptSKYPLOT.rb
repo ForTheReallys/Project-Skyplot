27ec5dbc354175c5f3f6035dbcf40bf6c60072ea
jmtman1216 <jmtman1216@yahoo.com>
2017-11-06 15:38:41 -0600

    more or less done
    until we meet of course and actually begin work

--- MainScriptSKYPLOT.rb
+++ MainScriptSKYPLOT.rb
@@ -1,6 +1,16 @@
 # Created by Justin Tran, Andrew Miller, Eamon Mulholland on November 6, 2017
 # This is a file to contain the script necessary to do the neccesary calculations for Skyplot
+=begin
+This is here to provide some form of an place to code puke.
+ feel free to edit and modify as needed.
+We can make a more organized awesome project as time goes on especially
+ for our 1st iteration
+=end
 class Skyplot
+    $point
+    $angle
+    #can make this shorter
+    $distance_in_lightyears
     def calculate_distance(Anlge,light_year)
 
         end
