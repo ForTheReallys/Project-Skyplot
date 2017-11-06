9fee9975948ddf804ddc55e438af64f3cee21926
jmtman1216 <jmtman1216@yahoo.com>
2017-11-06 15:29:09 -0600

    Skeleton
    Added skeletons that may or may not work for what we need ;)

--- MainScriptSKYPLOT.rb
+++ MainScriptSKYPLOT.rb
@@ -1,2 +1,13 @@
 # Created by Justin Tran, Andrew Miller, Eamon Mulholland on November 6, 2017
 # This is a file to contain the script necessary to do the neccesary calculations for Skyplot
+class Skyplot
+    def calculate_distance(Anlge,light_year)
+
+        end
+    def plot_points()
+
+        end
+    def get_point()
+        
+        end
+end
