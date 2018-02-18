08d97a7cd591e13d460b967fbba1b53e1ea4a574
jmtman1216 <31552045+jmtman1216@users.noreply.github.com>
2018-02-18 01:29:01 -0600

    added Readscript and the .xlsx
    first attempt to write excel reader,

--- Readscript.rb
+++ Readscript.rb
@@ -1,1 +1,9 @@
-
+#This is just basic script that reads an excel file
+#and input the data into an array
+require win32ole
+module readEx
+    excel=win32ole::new('excel.Application')
+    workbook = excel.Workbooks.Open('C:\Users\Jmtran1216\Documents\GitHub\Project-Skyplot\pyxis star coords.xlsx')
+    worksheet = workbook.Worksheets(1) #Positioning to the first sheet of the workbook
+    data = worksheet.Range('a3:l3')['Value']
+    
