b29227e0cfa701c08ed031df5017918b8e8d906f
jmtman1216 <31552045+jmtman1216@users.noreply.github.com>
2018-02-28 19:35:48 -0600

    got it running
    not to get it to do what i need it too....

--- Readscript.rb
+++ Readscript.rb
@@ -1,9 +1,26 @@
 #This is just basic script that reads an excel file
 #and input the data into an array
-require win32ole
-module readEx
-    excel=win32ole::new('excel.Application')
-    workbook = excel.Workbooks.Open('C:\Users\Jmtran1216\Documents\GitHub\Project-Skyplot\pyxis star coords.xlsx')
-    worksheet = workbook.Worksheets(1) #Positioning to the first sheet of the workbook
-    data = worksheet.Range('a3:l3')['Value']
+require 'roo' 
+module ReadEx
+  workbook = Roo::Spreadsheet.open 'C:\Users\Jmtran1216\Documents\GitHub\Project-Skyplot\pyxis star coords.xlsx'
+  worksheets = workbook.sheets
+  i = 0
+  j = 0
+  coords =[]
+  puts "Found #{worksheets.count} worksheets"
+  worksheets.each do |worksheet|
+        puts "Reading: #{worksheet}"
+        num_rows = 0
+        workbook.sheet(worksheet).each_row_streaming do |row|
+        row_cells = row.map { |cell| cell.value }
+        coords[i]= row_cells
+        j+=1
+        num_rows +=1
+      end
+      puts "Read #{num_rows} rows" 
+    end
+  while i<coords.length
+    puts coords[i]
+    i+=1
+    end
 end
