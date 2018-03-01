547e05af35c888bdfe45cdf310976ac70d3440c4
jmtman1216 <31552045+jmtman1216@users.noreply.github.com>
2018-02-28 19:47:33 -0600

    It works as intened
    I think

--- Readscript.rb
+++ Readscript.rb
@@ -1,20 +1,22 @@
 #This is just basic script that reads an excel file
-#and input the data into an array
+
 require 'roo' 
+#require 'sketchup.rb'
+#require 'extensions.rb'
+
 module ReadEx
   workbook = Roo::Spreadsheet.open 'C:\Users\Jmtran1216\Documents\GitHub\Project-Skyplot\pyxis star coords.xlsx'
   worksheets = workbook.sheets
   i = 0
   j = 0
-  coords =[]
+  coords =Array.new
   puts "Found #{worksheets.count} worksheets"
   worksheets.each do |worksheet|
         puts "Reading: #{worksheet}"
         num_rows = 0
         workbook.sheet(worksheet).each_row_streaming do |row|
         row_cells = row.map { |cell| cell.value }
-        coords[i]= row_cells
-        j+=1
+        coords[num_rows]= row_cells
         num_rows +=1
       end
       puts "Read #{num_rows} rows" 
