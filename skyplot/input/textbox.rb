a0295a1a3facd14d0c8a6a721e1a889047e9591a
Andrew Miller <andrew.miller14.am@gmail.com>
2018-05-03 13:10:54 -0500

    Add input subdirectory files

---
+++ skyplot/input/textbox.rb
@@ -0,0 +1,16 @@
+require 'skyplot/input/input'
+
+
+module Example::HtmlInputBox
+  module HtmlUI
+
+    class Textbox < Input
+
+      def initialize(label, default = nil)
+        super(label: label, default: default)
+      end
+
+    end # class
+
+  end # module
+end # module
