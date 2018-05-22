80a7a706de721d11cf02bc1e6584a35220be1de3
mill1893 <andrew.miller14.am@gmail.com>
2018-05-21 21:48:45 -0500

    Clean build of sketchup

---
+++ skyplot/input/dropdown.rb
@@ -0,0 +1,16 @@
+require 'skyplot/input/input'
+
+
+module Example::HtmlInputBox
+  module HtmlUI
+
+    class Dropdown < Input
+
+      def initialize(label, default = nil, options = [])
+        super(label: label, default: default, options: options)
+      end
+
+    end # class
+
+  end # module
+end # module
