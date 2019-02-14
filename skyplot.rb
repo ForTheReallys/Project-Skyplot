7ed54ef143ac47c48b2e4946925caf7eaba0ecb8
Alex Maese <memaese@hotmail.com>
2019-02-14 16:34:30 -0600

    Resolve unfinished merge conflict

--- skyplot.rb
+++ skyplot.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'sketchup.rb'
 require 'extensions.rb'
 
@@ -16,23 +15,4 @@ module ProjectSkyplot
     end
 
   end # module ProjectSkyplot
-=======
-require 'sketchup.rb'
-require 'extensions.rb'
-
-module ProjectSkyplot
-  module Skyplot
-
-    unless file_loaded?(__FILE__)
-      ex = SketchupExtension.new('Skyplot', 'skyplot/main')
-      ex.description = 'Utility to plot the points in a constellation.'
-      ex.version     = '1.0.0'
-      ex.copyright   = 'Team Skyplot © 2018'
-      ex.creator     = 'Andrew Miller'
-      Sketchup.register_extension(ex, true)
-      file_loaded(__FILE__)
-    end
-
-  end # module ProjectSkyplot
->>>>>>> b498f57db968744cc0cbd479f566e13702e1a470
 end # module Plotter
